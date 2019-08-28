e337edb033327c389fb49b0f9cb233025db8559b
Greg Doud <gpdoud@gmail.com>
2019-08-28 09:25:01 -0400

    Add project files.

---
+++ AccountClassProject/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AccountClassProject {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
